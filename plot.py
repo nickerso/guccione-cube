5c5a3533dc932507a6307ba08b380028d48be133
David Nickerson <nickerso@users.sourceforge.net>
2016-03-16 22:54:03 +1300

    tidy up the looping through parameter sets

--- plot.py
+++ plot.py
@@ -14,43 +14,34 @@ import json
 from subprocess import run, DEVNULL
 from pprint import pprint
 
-# having trouble with conflicting MKL DLLs if do this all in one script?!
-resultsFileName = os.path.abspath(os.path.join(Root, "results.json"))
-#default parameters - results
-run(["python", RunScript], stdout=DEVNULL)
-with open(resultsFileName) as resultsFile:
-    results = json.load(resultsFile)
+colours = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+colourIndex = 0
 
-labelText = str(results["materialParameters"])
-plt.plot(results["fibre"]["strain"], results["fibre"]["stress"], "b-", label=labelText)
-plt.plot(results["cross"]["strain"], results["cross"]["stress"], "b--")
+parameterSets = [
+                 ["1.0", "5.0", "10.0", "5.0"], #defaults
+                 ["0.25", "5.0", "10.0", "5.0"],
+                 ["0.5", "5.0", "10.0", "5.0"],
+                 ["0.75", "5.0", "10.0", "5.0"],
+                 ["1.25", "5.0", "10.0", "5.0"],
+                 ["1.5", "5.0", "10.0", "5.0"],
+                 ["1.75", "5.0", "10.0", "5.0"],
+                 ["2.0", "5.0", "10.0", "5.0"]
+                 ]
 
-run(["python", RunScript, "2.0", "5.0", "10.0", "5.0"], stdout=DEVNULL)
-with open(resultsFileName) as resultsFile:
-    results = json.load(resultsFile)
-
-labelText = str(results["materialParameters"])
-plt.plot(results["fibre"]["strain"], results["fibre"]["stress"], "g-", label=labelText)
-plt.plot(results["cross"]["strain"], results["cross"]["stress"], "g--")
-
-run(["python", RunScript, "1.0", "10.0", "10.0", "5.0"], stdout=DEVNULL)
-with open(resultsFileName) as resultsFile:
-    results = json.load(resultsFile)
-
-labelText = str(results["materialParameters"])
-plt.plot(results["fibre"]["strain"], results["fibre"]["stress"], "r-", label=labelText)
-plt.plot(results["cross"]["strain"], results["cross"]["stress"], "r1")
-
-run(["python", RunScript, "1.0", "5.0", "5.0", "5.0"], stdout=DEVNULL)
-with open(resultsFileName) as resultsFile:
-    results = json.load(resultsFile)
-
-labelText = str(results["materialParameters"])
-plt.plot(results["fibre"]["strain"], results["fibre"]["stress"], "c-", label=labelText)
-plt.plot(results["cross"]["strain"], results["cross"]["stress"], "co")
-
+for i, parameters in enumerate(parameterSets, 0):
+    resultsFileName = os.path.abspath(os.path.join(Root, "results-{:03d}.json".format(i+1)))
+    run(["python", RunScript] + parameters + [resultsFileName], stdout=DEVNULL)
+    with open(resultsFileName) as resultsFile:
+        results = json.load(resultsFile)
+    labelText = str(results["materialParameters"])
+    plt.plot(results["fibre"]["strain"], results["fibre"]["stress"], colours[colourIndex] + "-", label=labelText)
+    plt.plot(results["cross"]["strain"], results["cross"]["stress"], colours[colourIndex] + "1--")
+    colourIndex = colourIndex + 1
+    if colourIndex >= len(colours):
+        colourIndex = 0
 
 plt.ylim(ymax=50)
-plt.ylabel('some numbers')
+plt.ylabel('axial force')
+plt.xlabel("axial strain")
 plt.legend(loc=0)
 plt.show()
